478c3fe85820e4c085f120f7bb60602ba5d7e9f6
Pablo Zabala <could_2032@outlook.com>
2025-07-09 04:46:31 -0300

    aprendiendo typescript para react

--- src/components/BtnCard.tsx
+++ src/components/BtnCard.tsx
@@ -1,4 +1,8 @@
-export default function BtnCard ({onClick}){
+interface CardBtn {
+  onClick: ()=> void;
+}
+
+export default function BtnCard ({onClick}: CardBtn){
 
   return(
     <button className='btn btn-secondary  ms-3' onClick={onClick}>
--- src/components/BtnClose.tsx
+++ src/components/BtnClose.tsx
@@ -1,4 +1,7 @@
-export default function BtnClose({onClick }){
+interface Close {
+  onClick: () => void;
+}
+export default function BtnClose({onClick }: Close){
   return(
     <button className='btn btn-danger m-3' onClick={onClick}>
       Cerrar
